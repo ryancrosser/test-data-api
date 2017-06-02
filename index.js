2af154d4ecd53ad299bb5d711293e29399576a3c
Ryan Crosser <ryankcrosser@gmail.com>
2017-06-01 20:41:37 -0400

    Updated PORT reference

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares)
 server.use(router)
